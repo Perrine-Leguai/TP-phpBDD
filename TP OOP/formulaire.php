5f402c63b5cd06b247e52a332e0f0beaef823a7d
Perrine L <perrine.leguai@gmail.com>
2020-11-02 11:41:51 +0100

    new exo

---
+++ TP OOP/formulaire.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    </head>
+    <body>
+       
+                                    
+              
+
+        <!-- CATEGORIE -->
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="employes col-6 offset-3  m-2  ">
+                    <div class="row justify-content-md-center m-4">
+                        <h3 class="employes col-4   ">FORMULAIRE</h3>
+                    </div>
+                    <div class="row">
+                    
+                        <!-- FORMULAIRES D AJOUT -->
+                        <div class="col-12">
+                            <div >
+                                <form action="traitement.php?action=ajout"  method="POST">
+                                    
+                                        <div class="row justify-content-md-center">
+                                           
+                                            <!-- NOM & PRENOM -->
+                                            
+                                                <div class="  row justify-content-md-center m-2 ">
+                                                    <div class=" row mr-2">
+                                                        <div class="p-1"><label for="nom"> Nom : </label></div>
+                                                        <div><input type="text" name="nom" id="nom" placeholder="ex: Dupont "  ></div>
+                                                    </div>
+                                                    <div class=" row justify-content-md-center ml-2">
+                                                        <div class="p-1"><label for="prenom"> Prénom : </label></div>
+                                                        <div><input type="text" name="prenom" id="prenom" placeholder="ex: Valérie " ></div>
+                                                    </div>
+                                                </div>
+                                            
+
+                                            <!-- SUPERIEUR -->
+                                            <div class=" row justify-content-md-center col-12 m-2 ">        
+                                                <div clas="p-1"><label for="Email"> Email :</label></div>
+                                                <div ><input type="email" name="email" id="sup" placeholder="ex: abc.def@gmail.com" ></div>
+                                            </div>
+
+                                            
+
+                                            <!-- BOUTON -->
+                                            <div class=" row justify-content-md-center col-4 m-4">
+                                                <button type="submit"  class="btn btn-primary "> Ajouter </button>
+                                            </div>
+
+                                        </div>
+                                </form> 
+                            </div>
+                        </div> 
+                    </div>
+                </div>        
+            </div>
+        </div>
+    </body>
+</html>
---
+++ TP OOP/traitement.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+    
+<a href="formulaire.php"><button type="button" class="btn btn-primary btn-lg" >Remplir le questionnaire</button></a>
+    
+<!-- afficher tableau -->
+<table class="table stripped table-hover">
+    <thead class="thead-light table-bordered" >
+        <tr>
+            
+            <th> Nom </th>
+            <th> Prénom </th>
+            <th> Email </th>
+            
+            
+        </tr>
+    </thead>
+    <tfoot class="thead-light table-bordered" >
+        <tr>
+            <th> Nom </th>
+            <th> Prénom </th>
+            <th> Email </th>
+
+        </tr>
+    </tfoot>
+    <tbody>
+    <!-- LECTURE DE LA BDD -->
+        
+
+        
+                
+            
+    </tbody>
+</table>
+
+
+<a href="mdp.php"><button type="button" class="btn btn-primary btn-lg" >Remplir le formulaire</button></a>
+<!-- afficher tableau -->
+<table class="table stripped table-hover">
+    <thead class="thead-light table-bordered" >
+        <tr>
+            
+            <th> Email </th>
+            <th> MDP </th>
+           
+            
+            
+        </tr>
+    </thead>
+    <tfoot class="thead-light table-bordered" >
+        <tr>
+        <th> Email </th>
+            <th> MDP </th>
+
+        </tr>
+    </tfoot>
+    <tbody>
+
+</body>
+</html>
